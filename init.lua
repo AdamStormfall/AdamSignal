cad8a78a04ac0e30c1c2dd8bf752fe9522b3303d
Stormfall <69389005+AdamStormfall@users.noreply.github.com>
2023-06-28 21:28:27 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,24 @@
 local AdamSignal = {}
 
+local HttpService = game:GetService("HttpService")
+
 AdamSignal.ClassName = "AdamSignal"
 
-function AdamSignal:__newindex(Index, Value)
+function AdamSignal.new()
+  local Event = Instance.new("BindableEvent")
+  local Key = HttpService:GenerateGUID(false)
+
+  Event.Name = Key
   
+  return setmetatable({
+      Event = Event,
+      Key = Key,
+      Table = {},
+  }, AdamSignal)
+end
+
+function AdamSignal:__newindex(Index, Value)
+  if 
 end
 
 return AdamSignal
